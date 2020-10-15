744f9977bd91c07aeef8c2fe442d3cbbec58342d
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-15 16:13:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ovning2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string userInput = "";
+            userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "Måndag":
+                    Console.WriteLine("1");
+                    break;
+                case "Tisdag":
+                    Console.WriteLine("2");
+                    break;
+                case "Onsdag":
+                    Console.WriteLine("3");
+                    break;
+                case "Torsdag":
+                    Console.WriteLine("4");
+                    break;
+                case "Fredag":
+                    Console.WriteLine("5");
+                    break;
+                case "Lördag":
+                    Console.WriteLine("6");
+                    break;
+                case "Söndag":
+                    Console.WriteLine("7");
+                    break;
+                default:
+                    Console.WriteLine("felinput");
+                    break;
+
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
